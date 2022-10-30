c68a93761f47a55ea683ea7bf3d44c8e0ef0ff7a
UzeyirN <7qzjlm4@code.edu.az>
2022-10-30 23:51:19 +0400

    Task 3110

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+#region
+
+//int num = 10;
+//if (num == 0)
+//    console.writeline("0 deyeri gire bilmezsiniz");
+//else
+//{
+//    while (num != 0)
+//        if (num % 2 == 0)
+//        {
+//            num /= 2;
+//        }
+//        else if (num == 1)
+//        {
+//            console.writeline(" ikinin quvvetidir");
+//            break;
+//        }
+//        else
+//        {
+//            console.writeline("ikinin quvveti deyil");
+//            break;
+//        }
+//}
+//
+#endregion
+
+#region Month
+//string month = "November";
+//switch (month)
+
+//{
+//    case "December":
+//    case "January":
+//    case "February":
+//        Console.WriteLine("Season of winter ");
+//    break;
+//    case "March":
+//    case "April":
+//    case "May":
+//        Console.WriteLine("Season of spring ");
+//    break;
+//    case "June":
+//    case "July":
+//    case "August":
+//        Console.WriteLine("Season of summer ");
+//    break;
+//    case "September":
+//    case "October":
+//    case "November":
+//        Console.WriteLine("Season of autumn ");
+//    break;
+//    default:
+//        Console.WriteLine("Wrong");
+//    break;
+//}
+
+
+#endregion
+
+
---
+++ tasssk1.js
@@ -0,0 +1,66 @@
+
+//IKININ QUVVETI
+
+// let num =4;
+// if ( num == 0)
+// {
+//     console.log("0 deyeri gire bilmezsiniz")
+// }
+// else
+// {
+// while (num !=0) 
+// if ( num %2 ==0) 
+//  {
+//     num/=2;
+//  }
+//  else if (num == 1)
+//  {
+//     console.log ("2-in quvvetidir");
+//  break;
+//  }
+//  else
+//  {
+//     console.log ( "2-quvveti deyil");
+//     break;
+//  }
+// }
+
+
+
+
+// MONTH MESELESI
+
+// let month="November";
+// switch(month)
+
+// {
+//     case "December":
+//     case "January":
+//     case "February":
+//     console.log("Season of winter ")
+//     break;
+//     case "March":
+//     case "April":
+//     case "May":
+//     console.log("Season of spring ")
+//     break;
+//     case "June":
+//     case "July":
+//     case "August":
+//     console.log("Season of summer ")
+//     break;
+//     case "September":
+//     case "October":
+//     case "November":
+//     console.log("Season ofautumn ")
+//     break;
+//     default:
+//     console.log("Wrong")
+//     break;
+// }
+
+
+
+
+
+
